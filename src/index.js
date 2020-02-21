a8cb3e98ce240c06ce8d30813a0d58004d3bd894
fosquito <fosquitopirex@gmail.com>
2020-02-21 11:39:06 +0000

    Theming Boostrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './css/index.css';
-import './css/bootstrap-grid.min.css'
-import './css/bootstrap-reboot.min.css'
-import './css/bootstrap.min.css'
-
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
