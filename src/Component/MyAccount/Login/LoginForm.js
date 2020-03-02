5b535ef365cd8f6c963a3c261743b33883744810
fosquito <fosquitopirex@gmail.com>
2020-03-02 21:40:45 +0000

    Few changes on login form

--- src/Component/MyAccount/Login/LoginForm.js
+++ src/Component/MyAccount/Login/LoginForm.js
@@ -12,26 +12,26 @@ class LoginForm extends Component {
         return (
             <div className="container py-4">
                 <form>
-                    <div className="form-group social-login mx-auto py-1">
+                    <div className="form-group social-login mx-auto">
                         <FacebookLoginButton iconSize="20px" align="center">
                             <span style={{ fontSize: 16 }}>Entrar com Facebook</span>
                         </FacebookLoginButton>
                     </div>
-                    <div className="form-group social-login mx-auto py-1">
+                    <div className="form-group social-login mx-auto">
                         <GoogleLoginButton iconSize="20px" align="center">
                             <span style={{ fontSize: 16 }}>Entrar com Google</span>
                         </GoogleLoginButton>
                     </div>
-                    <div className="form-group social-login mx-auto py-1">
+                    <div className="form-group social-login mx-auto">
                         <TwitterLoginButton iconSize="20px" align="center">
                             <span style={{ fontSize: 16 }}>Entrar com Twitter</span>
                         </TwitterLoginButton>
                     </div>
-                    <div className="h6 text-muted py-4">OU</div>
-                    <div className="form-group social-login mx-auto py-1">
+                    <div className="h6 text-muted py-5">OU</div>
+                    <div className="form-group social-login mx-auto">
                         <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" />
                     </div>
-                    <div className="form-group social-login mx-auto py-1">
+                    <div className="form-group social-login mx-auto">
                         <input type="password" class="form-control" id="Password" placeholder="Password" />
                     </div>
                     <div className="social-login mx-auto py-4">
