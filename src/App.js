ab987b3ca0a568b648b8781e3cbb94954c7c3a03
fosquito <fosquitopirex@gmail.com>
2020-03-08 22:06:24 +0000

    Changes on struct of components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Navbar1 from './Component/Navbar/Navbar1'
 import TransitionController from './Component/TransitionController';
-import { MyContext } from './Component/MyContext'
+import { LoginContext } from './Component/Context/LoginContext'
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: false,
+      loginLogout: this.loginLogout
     }
   }
 
@@ -20,12 +21,11 @@ class App extends Component {
   }
 
   render() {
-    const {isLoggedIn} = this.state
     return (
-      <MyContext.Provider value={isLoggedIn}>
+      <LoginContext.Provider value={this.state}>
           <Navbar1 />
           <TransitionController />
-      </MyContext.Provider>
+      </LoginContext.Provider>
     )
   }
 }
--- src/Component/MyAccount/LoginRegister/LoginForm.js
+++ src/Component/MyAccount/LoginRegister/LoginForm.js
@@ -5,51 +5,54 @@ import {
     TwitterLoginButton,
 } from "react-social-login-buttons"
 import { Link } from 'react-router-dom'
-import { MyContext } from '../../MyContext'
+import { LoginContext } from '../../Context/LoginContext'
 
 class LoginForm extends Component {
     render() {
-        console.log(this.context)
         return (
-            <div className="container py-4">
-                <form>
-                    <div className="form-group social-login mx-auto">
-                        <FacebookLoginButton iconSize="20px" align="center">
-                            <span style={{ fontSize: 16 }}>Entrar com Facebook</span>
-                        </FacebookLoginButton>
+            <LoginContext.Consumer>
+                {(props) => (
+                    <div className="container py-4">
+                        <form>
+                            <div className="form-group social-login mx-auto">
+                                <FacebookLoginButton iconSize="20px" align="center">
+                                    <span style={{ fontSize: 16 }}>Entrar com Facebook</span>
+                                </FacebookLoginButton>
+                            </div>
+                            <div className="form-group social-login mx-auto">
+                                <GoogleLoginButton iconSize="20px" align="center">
+                                    <span style={{ fontSize: 16 }}>Entrar com Google</span>
+                                </GoogleLoginButton>
+                            </div>
+                            <div className="form-group social-login mx-auto">
+                                <TwitterLoginButton iconSize="20px" align="center">
+                                    <span style={{ fontSize: 16 }}>Entrar com Twitter</span>
+                                </TwitterLoginButton>
+                            </div>
+                            <div className="h6 text-muted py-5">OU</div>
+                            <div className="form-group social-login mx-auto">
+                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" />
+                            </div>
+                            <div className="form-group social-login mx-auto">
+                                <input type="password" className="form-control" id="Password" placeholder="Password" />
+                            </div>
+                            <div className="social-login p-3">
+                                <div className="p text-justify" style={{ fontSize: 14 }}>
+                                    <Link to={''}>Esqueceu-se da password?</Link>
+                                </div>
+                            </div>
+                            <div className="social-login mx-auto pt-3">
+                                <button type="submit" className="btn btn-primary w-50" onClick={e => {
+                                    e.preventDefault()
+                                }}>Entrar</button>
+                            </div>
+                        </form>
                     </div>
-                    <div className="form-group social-login mx-auto">
-                        <GoogleLoginButton iconSize="20px" align="center">
-                            <span style={{ fontSize: 16 }}>Entrar com Google</span>
-                        </GoogleLoginButton>
-                    </div>
-                    <div className="form-group social-login mx-auto">
-                        <TwitterLoginButton iconSize="20px" align="center">
-                            <span style={{ fontSize: 16 }}>Entrar com Twitter</span>
-                        </TwitterLoginButton>
-                    </div>
-                    <div className="h6 text-muted py-5">OU</div>
-                    <div className="form-group social-login mx-auto">
-                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" />
-                    </div>
-                    <div className="form-group social-login mx-auto">
-                        <input type="password" className="form-control" id="Password" placeholder="Password" />
-                    </div>
-                    <div className="social-login p-3">
-                        <div className="p text-justify" style={{ fontSize: 14 }}>
-                            <Link to={''}>Esqueceu-se da password?</Link>
-                        </div>
-                    </div>
-                    <div className="social-login mx-auto pt-3">
-                        <button type="submit" className="btn btn-primary w-50" onClick={e => {
-                            e.preventDefault()
-                        }}>Entrar</button>
-                    </div>
-                </form>
-            </div>
+                )}
+            </LoginContext.Consumer>
         )
     }
 }
 
-LoginForm.contextType = MyContext
+LoginForm.contextType = LoginContext
 export default LoginForm
